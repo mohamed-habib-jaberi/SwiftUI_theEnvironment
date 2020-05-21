28f19102608abade8f85edede070283840bf59c3
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-05-21 20:34:44 +0200

    Initial Commit

---
+++ SwiftUI_theEnvironment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_theEnvironment
+//
+//  Created by mohamed  habib on 21/05/2020.
+//  Copyright © 2020 mohamed  habib. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
